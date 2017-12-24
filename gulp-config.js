ce69f0edfd5b2cd39e05a7f39d94cdf8eb27320b
Suresh Kumar Devaraj <dpsures@gmail.com>
2017-12-24 21:44:39 +0530

    first commit

---
+++ gulp-config.js
@@ -0,0 +1,8 @@
+module.exports = function(){
+    var config = {
+        allTs: './src/app/**/*.ts',
+        typings: './typings/**/.d.ts',
+        tsOutputPath: './dist'
+    }
+    return config;
+}
---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var tsc = require('gulp-typescript');
+var tslint = require('gulp-tslint');
+var tsProject = tsc.createProject('tsconfig.json');
+var config = require('./gulp-config')();
+
+var browserSync = require('browser-sync');
+var superstatic = require('superstatic');
+
+gulp.task('ts-lint', function(){
+    return gulp.src(config.allTs)
+                .pipe(tslint({
+                    formatter: 'prose'
+                }))
+                .pipe(tslint.report({
+                    emitError: false
+                }));
+});
+
+gulp.task('compile-ts', function(){
+    var sourceTsFiles = [
+        config.allTs,
+        config.typings
+    ];
+
+    var tsResult = gulp
+                    .src(sourceTsFiles)
+                    .pipe(sourcemaps.init())
+                    .pipe(tsProject());
+
+    return tsResult.js
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.tsOutputPath));
+});
+
+gulp.task('serve', ['ts-lint', 'compile-ts'], function(){
+
+    gulp.watch([config.allTs], ['ts-lint', 'compile-ts']);
+
+    browserSync({
+        port: 3000,
+        files: ['index.html', '**/*.js'],
+        injectChanges: true,
+        logFileChanges: false,
+        logLevel: 'silent',
+        notify: true,
+        reloadDelay: 0,
+        server: {
+            baseDir: './dist',
+            middleware: superstatic({debug: false})
+        }
+    });
+});
+
+gulp.task('default', ['serve']);
